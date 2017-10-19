dd57423066e94668109e730f29ec853e9ba3d422
Kang Ki Tae <kt.kang@ridi.com>
2017-10-19 12:31:58 +0900

    Override docker hub build command.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM alpine:latest
-MAINTAINER "Jonatan Bjork <jonatan@jonatanblue.se>"
+MAINTAINER "Kang Ki Tae <kt.kang@ridi.com>"
 
-ENV TERRAFORM_VERSION=0.9.11
+ARG TERRAFORM_VERSION=0.9.11
+ARG TERRAFORM_SHA256SUM=804d31cfa5fee5c2b1bff7816b64f0e26b1d766ac347c67091adccc2626e16f3
+
 ENV TERRAFORM_FILENAME=terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 ENV TERRAFORM_URL=https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/${TERRAFORM_FILENAME}
-ENV TERRAFORM_SHA256SUM=804d31cfa5fee5c2b1bff7816b64f0e26b1d766ac347c67091adccc2626e16f3
 
 RUN apk add --update git bash wget
 
---
+++ bin/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "${TERRAFORM_VERSION}" ]; then
+    TERRAFORM_VERSION=0.11.3
+fi
+
+CONTAINER_ARCHITECTURE=linux_amd64
+TERRAFORM_SHA256SUM=`curl https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS --silent | grep ${CONTAINER_ARCHITECTURE} | awk '{ print $1 }'`
+
+echo "=> Building start with args"
+echo "CONTAINER_ARCHITECTURE=${CONTAINER_ARCHITECTURE}"
+echo "TERRAFORM_VERSION=${TERRAFORM_VERSION}"
+echo "TERRAFORM_SHA256SUM=${TERRAFORM_SHA256SUM}"
+
+docker build \
+  --build-arg CONTAINER_ARCHITECTURE=${CONTAINER_ARCHITECTURE} \
+  --build-arg TERRAFORM_VERSION=${TERRAFORM_VERSION} \
+  --build-arg TERRAFORM_SHA256SUM=${TERRAFORM_SHA256SUM} \
+  -t ridibooks/gitlab-ci-terraform:${TERRAFORM_VERSION} .
+
+docker tag ridibooks/gitlab-ci-terraform:${TERRAFORM_VERSION} ridibooks/gitlab-ci-terraform:latest
